ca2f4fb3742207cd72fe759398dc17850b4d4dfd
@githubAdmin123 <certgendict@gmail.com>
2023-04-19 16:37:26 +0800

    navbar editted

--- dict-wd-mambu/src/Navbar.js
+++ dict-wd-mambu/src/Navbar.js
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
-export default function Nabvar() {
-    return(
-        <nav>
-      <ul>
-        <li><Link to="/">Login</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/add-webinar">Add Webinar</Link></li>
-        <li><Link to="/webinar-list">Webinar List</Link></li>
-        <li><Link to="/participant-management">Participants Management</Link></li>
-      </ul>
-    </nav>
-    )
+export default function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">My App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/dashboard" className="nav-link">Dashboard</Link>
+            <Link to="/add-webinar" className="nav-link">Add Webinar</Link>
+            <Link to="/webinar-list" className="nav-link">Webinar List</Link>
+            <Link to="/participant-management" className="nav-link">Participants Management</Link>
+          </Nav>
+          <Nav>
+            <Link to="/" className="nav-link">Login</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
--- dict-wd-mambu/src/index.js
+++ dict-wd-mambu/src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./styles.css"
 import { BrowserRouter } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
