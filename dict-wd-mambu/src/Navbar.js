30ce6abb7ab8cd8b745f27d0fd373e27c1e4e76f
@githubAdmin123 <certgendict@gmail.com>
2023-04-19 16:56:40 +0800

    its 12 in the morning

--- dict-wd-mambu/src/Navbar.js
+++ dict-wd-mambu/src/Navbar.js
@@ -1,21 +1,23 @@
 import { Link } from 'react-router-dom';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
 export default function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">My App</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">DICT Cert Gen</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Link to="/dashboard" className="nav-link">Dashboard</Link>
-            <Link to="/add-webinar" className="nav-link">Add Webinar</Link>
-            <Link to="/webinar-list" className="nav-link">Webinar List</Link>
+            <NavDropdown title="Webinar Management" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/add-webinar">Add Webinar</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/webinar-list">Webinar List</NavDropdown.Item>
+            </NavDropdown>
             <Link to="/participant-management" className="nav-link">Participants Management</Link>
           </Nav>
           <Nav>
-            <Link to="/" className="nav-link">Login</Link>
+            <Link to="/" className="nav-link">Logout</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
