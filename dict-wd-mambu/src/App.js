9fe6bdd1e74bcd1d19f942d95942c1890636e95a
@githubAdmin123 <certgendict@gmail.com>
2023-04-24 15:37:49 +0800

    asdasdasd

--- dict-wd-mambu/src/App.js
+++ dict-wd-mambu/src/App.js
@@ -5,12 +5,14 @@ import AddWebinar from "./pages/AddWebinar"
 import WebinarList from "./pages/WebinarList"
 import ParticipantManagement from "./pages/ParticipantManagement"
 import  {Route, Routes} from "react-router-dom"
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <><Nabvar />
     <div className="container">
       <Routes>
+        <Route exact path="/" component={LoginPage} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/add-webinar" element={<AddWebinar />} />
         <Route path="/webinar-list" element={<WebinarList />} />
---
+++ dict-wd-mambu/src/pages/LoginPage.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { supabase } from '../supabaseClient';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    const { user, error } = await supabase.auth.signIn({
+      email: email,
+      password: password,
+    });
+    if (error) {
+      setError(error.message);
+    } else {
+      // User is signed in, redirect to protected content
+      window.location.href = '/dashboard';
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Log in</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
